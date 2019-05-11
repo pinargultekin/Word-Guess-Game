2522efc2b3e568492d6282e556fd9a88378cbaa1
29bucuk <pinargultekin@yahoo.com>
2019-05-10 21:12:50 -0400

    Variables added to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var words = ["wannabe" , "what is love", "macarena", "i like to move it", "mambo no five", "wonderwall", "i will always love you", "no diggity", "all star", "ice ice baby", "under the bridge", "bitter sweet symphony"];
+var wins = 0;
+var maxGuess = 13;
+var currentWord = [];
+var userGuess = [];
+var match;
+var remainingGuess;
+
+
