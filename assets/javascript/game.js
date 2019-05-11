592347b0a8b6f4e14c6047dcd9e1f7dfb04549c1
29bucuk <pinargultekin@yahoo.com>
2019-05-11 19:35:52 -0400

    key event added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,49 +3,14 @@
 var words = ["wannabe" , "what is love", "macarena", "i like to move it", "mambo no five", "wonderwall", "i will always love you", "no diggity", "all star", "ice ice baby", "under the bridge", "bitter sweet symphony"];
 var currentWord = "";
 var currentWordLetter = [];
-var blankNum = 0;
+var blank = 0;
 var match = [];
 var incorrectGuess = [];
 
 var wins = 0;
 var remainingGuess = 0;
 
-function game() {
-    currentWord = words[Math.floor(Math.random() * words.length)];
-    console.log(currentWord);
-
+document.onkeyup = function(event) {
+    var guessedLetter = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(guessedLetter);
 }
-
-
-//display underscored current word  
-
-//listen the key event
-
-// letter check
-// match
-// change the underscore to letter
-if ( userGuess === currentWord) {
-    
-}
-
-//not change guess remaining
-
-
-// if the last letter guessed correctly change the score +1
-
-
-//reset the game
-
-
-// not match
-
-//show the guessed letter under the letters already gueesed section
-
-
-//if guess remaining = 0 prompt  you lost
-
-
-//display new game
-
-};
-
